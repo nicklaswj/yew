9f07aca5c64ec0e30196110a8ba47271552d634f
Martin Linhard <linmad17@htl-kaindorf.at>
2020-05-16 19:47:19 +0200

    tried to refactor the poll_next implementation

--- yew/src/services/fetch/web_sys_stream/mod.rs
+++ yew/src/services/fetch/web_sys_stream/mod.rs
@@ -5,13 +5,13 @@ pub mod readable_stream;
 /// ReadableStreamDefaultReader and ReadableStreamDefaultReaderValue
 pub mod readable_stream_default_reader;
 
-use std::future::Future;
-use wasm_bindgen_futures::JsFuture;
-use wasm_bindgen::JsValue;
+use futures::ready;
 use futures::stream::Stream;
 use futures::task::{Context, Poll};
-use futures::ready;
+use std::future::Future;
 use std::pin::Pin;
+use wasm_bindgen::JsValue;
+use wasm_bindgen_futures::JsFuture;
 
 struct YewStream {
     inner: Option<ReadableStreamDefaultReader>,
@@ -31,25 +31,55 @@ impl Stream for YewStream {
     type Item = Result<JsValue, JsValue>;
 
     fn poll_next(self: Pin<&mut Self>, cx: &mut Context) -> Poll<Option<Self::Item>> {
+        /*
+        //next_val = none
         if self.next_val.is_none() {
+            //inner = some; next_val = none
             if let Some(inner) = self.inner {
                 match inner.read() {
                     Ok(future_read) => {
-                        self.get_mut().next_val = Some(
-                            future_read
-                        );
+                        self.get_mut().next_val = Some(future_read);
                     }
                     Err(err) => return Poll::Ready(Err(err)),
                 }
             } else {
+                //inner = none; next_val = none
                 return Poll::Ready(None);
             }
         }
 
+        //inner = ?; next_val = some
         let result = ready!(self.next_val.unwrap());
         self.next_val = None;
 
-        Poll::Ready(result)
+        Poll::Ready(result)*/
+
+        match (inner, next_val) = (self.inner, self.next_val) {
+            //inner = some, next_val = none
+            (Some(inner_v), None) => {
+                match inner.read() {
+                    Ok(future_read) => {
+                        self.get_mut().next_val = Some(future_read);
+                        let result = ready!(self.next_val.unwrap());
+                        self.next_val = None;
+
+                        Poll::Ready(result)
+                    }
+                    Err(err) => return Poll::Ready(Err(err)),
+                }
+            }
+            //inner = none, next_val = none
+            (None, None) => {
+                Poll::Ready(None)
+            }
+            //inner = ?, next_val = some
+            (_, Some(next_val_v)) => {
+                let result = ready!(self.next_val.unwrap());
+                self.next_val = None;
+
+                Poll::Ready(result)
+            }
+        }
     }
 }
 
@@ -58,7 +88,7 @@ pub struct ReadableStreamDefaultReader {
 }
 
 impl ReadableStreamDefaultReader {
-     pub fn closed(&self) -> Future<Output = Result<JsValue, JsValue>> {
+    pub fn closed(&self) -> Future<Output = Result<JsValue, JsValue>> {
         JsFuture::from(self.raw.closed())
-     }
+    }
 }
